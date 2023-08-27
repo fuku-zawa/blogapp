c36a7470535e0a5145974eb0378c7107c7598609
ken <e.kfc47@gmail.com>
2023-08-28 05:48:14 +0900

    aboutページにhomeリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#  /が来たら、homeのindex（= HomeControllerのindexメソッド）を実行する
-  get '/' => 'home#index'
+  # to:は省略可　rootはルートドメインにアクセスしたときに表示するページの指定
+  # getは後ろにパス指定できるが、ルートできない
+  root to: "home#index"
+  # /が来たら、homeのindex（= HomeControllerのindexメソッド）を実行する
+  # get '/' => 'home#index'
   get "/about" => "home#about"
 end
