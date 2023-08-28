0cc0a7a67b6d508913d0b33de11ab2d4c4eae7d0
ken <e.kfc47@gmail.com>
2023-08-29 05:50:33 +0900

    articlesコントローラに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # to:は省略可　rootはルートドメインにアクセスしたときに表示するページの指定
   # getは後ろにパス指定できるが、ルートできない
-  root to: "home#index"
+  root to: "articles#index"
   # /が来たら、homeのindex（= HomeControllerのindexメソッド）を実行する
   # get '/' => 'home#index'
-  get "/about" => "home#about"
+  
 end
