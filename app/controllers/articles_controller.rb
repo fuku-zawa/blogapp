95e6eed59bbd3d3c02bd0a1248d3ec26057bb475
ken <e.kfc47@gmail.com>
2023-09-03 09:28:24 +0900

    commentの追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
     # そのGETリクエストを受け取ると、paramsが生成され、そのidを↓で取得したいからparam[:id]
     def show
         # set_article
+        @comments = @article.comments
     end
 
     def new
