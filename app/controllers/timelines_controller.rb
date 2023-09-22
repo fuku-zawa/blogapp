adde4de1edaef6ea4d6cd18262764168cb45414c
ken <e.kfc47@gmail.com>
2023-09-23 04:37:14 +0900

    テンプレート共通化

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,9 @@
 class TimelinesController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
-    
+    # followingで取得できたidをすべて取得する（配列が入る）
+    user_ids = current_user.followings.pluck(:id)
+    @articles = Article.where(user_id: user_ids)
   end
 end
