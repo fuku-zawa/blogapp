fbd9cee0c46e91e4d1e7ab2fa0fef23f2489a785
ken <e.kfc47@gmail.com>
2023-09-24 05:49:26 +0900

    コメント一覧表示、投稿ボタンの設定

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -15,6 +15,24 @@ const handleHeartDisplay = (hasLiked) => {
 document.addEventListener('turbolinks:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
+
+  axios.get(`/articles/${articleId}/comments`)
+    .then((response) => {
+      const comments = response.data
+      comments.forEach((comment) => {
+        $('.comments-container').append(
+          `<div class="article_comment"><p>${comment.content}</p></div>`
+        )
+      })
+    })
+
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-textarea').removeClass('hidden')
+  })
+
+
+
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
